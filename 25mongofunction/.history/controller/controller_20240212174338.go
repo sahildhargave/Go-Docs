073c684001b51e2c7306d7229f9f32dc0f7864e1
Sahil Dhargave <sahildhargave5253@gmail.com>
2024-02-13 01:17:23 +0530

    connectiong with MongoDB

---
+++ 25mongofunction/.history/controller/controller_20240212174338.go
@@ -0,0 +1,51 @@
+package controller
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const connectionString = "mongodb+srv://sahildhargave5253:1KJk5mrwbJeGlq0d@cluster0.gpxr5wx.mongodb.net/?retryWrites=true&w=majority"
+
+const dbName = "Netflix"
+const colName = "watchlist"
+
+// most important'
+var collection *mongo.Collection
+
+//connect with mongoDB
+
+func init() {
+	//client option
+	clientOption := options.Client().ApplyURI(connectionString)
+
+	// connect to mongoDB
+	client, err := mongo.Connect(context.TODO(), clientOption)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("MongoDB conntection success")
+	collection = client.Database(dbName).Collection(colName)
+
+	//collection instance
+	fmt.Println("Collection instance is ready")
+}
+
+//MONGODB helpers -file
+
+// insert one record
+
+func insertOneMovie(movie model.Netflix) {
+	inserted, err := collection.InsertOne(context.TODO(), movie)
+    
+	if err!=nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted 1 movie in db with id:", inserted)
+}
