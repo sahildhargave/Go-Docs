349e98751d293308eb06d32daa183946fa4a9316
Sahil Dhargave <sahildhargave5253@gmail.com>
2024-02-14 11:30:02 +0530

    CRUD Operation

--- 25mongofunction/controller/controller.go
+++ 25mongofunction/controller/controller.go
@@ -2,9 +2,14 @@ package controller
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 
+	"github.com/sahildhargave/25mongodb/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -48,4 +53,92 @@ func insertOneMovie(movie model.Netflix) {
 		log.Fatal(err)
 	}
 	fmt.Println("Inserted 1 movie in db with id:", inserted.InsertedID)
+}
+
+// update 1 record
+//should by hard learn and keep in mind
+//TODO - Difference between bson.m and bson.D
+// from stackoverflow
+
+func updateOnMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	//😀😁 THERE ARE TWO BSON.M and BSON.D
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"watched": true}}
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Modified %v documents\n", result.ModifiedCount)
+}
+
+func deleteOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+	deleteCount, err := collection.DeleteOne(context.Background(), filter, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Movie got delete with delete count:", deleteCount)
+}
+
+// 👽👾🤖💩😺😸😹😻😼😽🙀
+// Delete all records from mongodb
+
+func deleteAllMovie() int64 {
+	//😁 if not having any value in {} then {{}} --> select every things
+	//filter := bson.D{{}}
+	// most of the go developer prefer
+	deleteResult, err := collection.DeleteMany(context.Background(), bson.D{{}}, nil)
+	//   collection.DeleteMany(context.Background(), filter)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Number of movies delete:", deleteResult.DeletedCount)
+	return deleteResult.DeletedCount
+
+}
+
+
+// get all movie from databases
+
+func getAllMovies() []primitive.M{
+	cur, err := collection.Find(context.Background(), bson.D{{}})
+
+	if err!= nil {
+		log.Fatal(err)
+	}
+	 var movies []primitive.M
+
+	 for cur.Next(context.Background()){
+		var movie bson.M
+		err := cur.Decode( &movie )
+		if err!= nil {
+			log.Fatal(err)
+		}
+		movies = append(movies, movie)
+	 }
+
+	 defer cur.Close(context.Background())
+
+	 return movies
+}
+
+// A😁😂🤣Actual controller -file
+
+func GetMyAllMovies(w http.ResponseWriter, r *http.Request){
+ w.Header().Set("Content-Type","application/x-www-form-urlencoded") //set header
+ allMovies := getAllMovies()
+ json.NewEncoder(w).Encode(allMovies)
+}
+
+// 😁😂 create function
+func CreateMovie(w http.ResponseWriter, r *http.Request){
+  w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+  w.Header().Set("Allow-Control-Allow-Methods","POST")
+  var movie model.NewEncoder
 }
--- 25mongofunction/model/models.go
+++ 25mongofunction/model/models.go
@@ -1,6 +1,8 @@
 package model
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import(
+	 "go.mongodb.org/mongo-driver/bson/primitive"
+	)
 
 type Netflix struct {
 	ID      primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
