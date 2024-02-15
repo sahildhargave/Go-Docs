538e4fb546f9be363657f163d92f45d35aa3a8e7
Sahil Dhargave <sahildhargave5253@gmail.com>
2024-02-15 18:59:22 +0530

    working with sync

--- 26goroutines/main.go
+++ 26goroutines/main.go
@@ -2,26 +2,37 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
+	"sync"
 )
 
+var wg sync.WaitGroup //pointer
 func main() {
-	go greeter("Hello")
-	greeter("world")
-	go greateNight("earth")
+	websitelist := []string{
+		"https://youtube.com",
+		"http://go.dev",
+		"https://google.com",
+		"https://github.com",
+		"https://chatgtp.com",
+	}
 
+	for _, web := range websitelist {
+		getStatusCode(web)
+		wg.Add(1)
+	}
+
+	wg.Wait()
 }
 
-func greeter(s string) {
-	for i := 0; i < 6; i++ {
-		time.Sleep(3 * time.Millisecond)
-		fmt.Println(s)
-	}
-}
+func getStatusCode(endpoint string) {
+	defer wg.Done()
 
-func greateNight(s string) {
-	for i := 0; i < 6; i++ {
-		time.Sleep(3 * time.Millisecond)
-		fmt.Println("Sahil Dhargave")
+	res, err := http.Get(endpoint)
+
+	if err != nil {
+		fmt.Println("OOPS in endpoint")
+	} else {
+		fmt.Printf("200 status code for website is %s", res.StatusCode, endpoint)
 	}
+
 }
